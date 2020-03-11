dda601f91eb884d6cc4ce763c1e957398d5409b7
presencewebdesign <presenceGithub84>
2020-03-11 09:07:47 +0000

    add switch for projects

--- components/projects.js
+++ components/projects.js
@@ -9,20 +9,23 @@ module.exports = function(type) {
     console.log(`Build Type ${type} ${argv.type}`);
     const argvType = argv.type
 
-    if(argvType === "abandonment") {
-        folder_src = paths.src_assets_aband_folder;
-        folder_dist = paths.dist_assets_aband_folder;
+    switch (argvType) {
+        case 'abandonment':
+            folder_src = paths.src_assets_aband_folder;
+            folder_dist = paths.dist_assets_aband_folder;
+        break;
+        case 'quickquote':
+            folder_src = paths.src_assets_qq_folder;
+            folder_dist = paths.dist_assets_qq_folder;
+        break;
+        case 'hospital':
+            folder_src = paths.hospitalfinder_src_folder;
+            folder_dist = paths.hospitalfinder_dist_folder;
+        break;
+        default:
+            folder_src = paths.src_folder;
+            folder_dist = paths.dist_folder;
     }
-    
-    if(argvType === "quickquote") {
-        folder_src = paths.src_assets_qq_folder;
-        folder_dist = paths.dist_assets_qq_folder;
-    }
-
-    if(argvType === "hospital") {
-        folder_src = paths.hospitalfinder_src_folder;
-        folder_dist = paths.hospitalfinder_dist_folder;
-    }
-
+      
     return type == "src" ? folder_src : folder_dist
 }
