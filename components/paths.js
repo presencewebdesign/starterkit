0aefa8ef42fa1bc676753b0a460c95ad0c4a4cb5
presencewebdesign <presenceGithub84>
2020-03-20 11:59:00 +0000

    add custom project

--- components/paths.js
+++ components/paths.js
@@ -1,22 +1,14 @@
 // Set folder paths
 module.exports = {
-    src_folder                  :'./src/',
-    src_assets_folder           :'./src/assets/',
-    dist_folder                 :'./dist/',
-    dist_folder_clear           :'./dist/*',
-    dist_assets_folder          :'./dist/assets/',
-    node_modules_folder         :'./node_modules/',
-    dist_node_modules_folder    :'./dist/node_modules/',
+    src_folder: "./src/",
+    src_assets_folder: "./src/assets/",
+    dist_folder: "./dist/",
+    dist_folder_clear: "./dist/*",
+    dist_assets_folder: "./dist/assets/",
+    node_modules_folder: "./node_modules/",
+    dist_node_modules_folder: "./dist/node_modules/",
 
-    // Form Hospital Finder
-    hospitalfinder_src_folder   :'./src/hospital/',
-    hospitalfinder_dist_folder  :'./dist/hospital/',
-
-    // Form Abandonment
-    src_assets_aband_folder     :'./src/abandonment/',
-    dist_assets_aband_folder    :'./dist/abandonment/',
-
-    // Quick Quote src & dist
-    src_assets_qq_folder        :'./src/quickQuote/',
-    dist_assets_qq_folder       :'./dist/quickquote/'
-}
+    // Your Project
+    project_src_folder: "./src/project/",
+    project_dist_folder: "./dist/project/"
+};
--- components/projects.js
+++ components/projects.js
@@ -1,31 +1,21 @@
-const argv = require('yargs').argv
-const paths = require('../components/paths');
+const argv = require("yargs").argv;
+const paths = require("../components/paths");
 
 var folder_src = paths.src_folder;
 var folder_dist = paths.dist_folder;
 
 module.exports = function(type) {
-
     console.log(`Build Type ${type} ${argv.type}`);
-    const argvType = argv.type
-
+    const argvType = argv.type;
     switch (argvType) {
-        case 'abandonment':
-            folder_src = paths.src_assets_aband_folder;
-            folder_dist = paths.dist_assets_aband_folder;
-        break;
-        case 'quickquote':
-            folder_src = paths.src_assets_qq_folder;
-            folder_dist = paths.dist_assets_qq_folder;
-        break;
-        case 'hospital':
-            folder_src = paths.hospitalfinder_src_folder;
-            folder_dist = paths.hospitalfinder_dist_folder;
-        break;
+        case "project":
+            folder_src = paths.project_src_folder;
+            folder_dist = paths.project_dist_folder;
+            break;
         default:
             folder_src = paths.src_folder;
             folder_dist = paths.dist_folder;
     }
-      
-    return type == "src" ? folder_src : folder_dist
-}
+
+    return type == "src" ? folder_src : folder_dist;
+};
